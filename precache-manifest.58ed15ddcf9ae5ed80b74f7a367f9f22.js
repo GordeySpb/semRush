09ddce362e5fcebec42dad14b5f51d04dc2a0b3e
GordeySpb <gordey_89@mail.ru>
2020-02-02 22:45:57 +0300

    Updates

---
+++ precache-manifest.58ed15ddcf9ae5ed80b74f7a367f9f22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2e323964bc819230de731ca28a3f94f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66cfdc466879b88baa7e",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "3dc0b7f6a8d3baba386d",
+    "url": "/static/css/main.ef75ce9c.chunk.css"
+  },
+  {
+    "revision": "66cfdc466879b88baa7e",
+    "url": "/static/js/2.f0cbad0c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.f0cbad0c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3dc0b7f6a8d3baba386d",
+    "url": "/static/js/main.86b4c20f.chunk.js"
+  },
+  {
+    "revision": "b24f5aa73eedb9c3d017",
+    "url": "/static/js/runtime-main.727a590d.js"
+  }
+]);
